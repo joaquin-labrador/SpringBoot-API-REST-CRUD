0818813c891edcadba0f6d4b9029806cfa35b599
joaquin-labrador <joaquinlabrador1010@gmail.com>
2023-04-16 00:56:35 -0300

    Exceptions catch in the controller advice

--- src/main/java/com/example/obrestdata_jpa/Controllers/BookController.java
+++ src/main/java/com/example/obrestdata_jpa/Controllers/BookController.java
@@ -2,6 +2,7 @@ package com.example.obrestdata_jpa.Controllers;
 
 import ch.qos.logback.classic.Logger;
 import com.example.obrestdata_jpa.Entities.Book;
+import com.example.obrestdata_jpa.Error.BookNotFoundException;
 import com.example.obrestdata_jpa.Services.BookService;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -36,17 +37,13 @@ public class BookController {
     @GetMapping("/api/books")
     @ResponseBody
     public ResponseEntity<List<Book>> findAll() {
-        try {
-            List<Book> books = this.bookService.findAll();
-            if (books.isEmpty()) {
-                return ResponseEntity.noContent().build();
-            } else {
-                return ResponseEntity.ok(books);
-            }
-        } catch (Exception e) {
-            logger.error("Error al buscar libros: " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        List<Book> books = this.bookService.findAll();
+        if (books.isEmpty()) {
+            throw new BookNotFoundException("No hay libros");
+        } else {
+            return ResponseEntity.ok(books);
         }
+
     }
 
     /*
@@ -57,16 +54,12 @@ public class BookController {
     @GetMapping("/api/books/{id}")
     @ResponseBody
     public ResponseEntity<Book> findById(@PathVariable UUID id) {
-        try {
-            Book book = this.bookService.findById(id);
-            if (book == null) {
-                return ResponseEntity.notFound().build();
-            } else {
-                return ResponseEntity.ok(book);
-            }
-        } catch (Exception e) {
-            logger.error("Error al buscar libro: " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+
+        Book book = this.bookService.findById(id);
+        if (book == null) {
+            throw new BookNotFoundException("Libro no encontrado");
+        } else {
+            return ResponseEntity.ok(book);
         }
 
     }
@@ -96,17 +89,14 @@ public class BookController {
     @PutMapping("/api/books/{id}")
     @ResponseBody
     public ResponseEntity<Book> update(@PathVariable UUID id, @RequestBody Book book) {
-        try {
-            Book bookToUpdate = this.bookService.update(id, book);
-            if (bookToUpdate == null) {
-                return null;
-            } else {
-                return ResponseEntity.ok(bookToUpdate);
-            }
-        } catch (Exception e) {
-            logger.error("Error al actualizar libro: " + e.getMessage());
-            return null;
+
+        Book bookToUpdate = this.bookService.update(id, book);
+        if (bookToUpdate == null) {
+            throw new BookNotFoundException("Libro no encontrado");
+        } else {
+            return ResponseEntity.ok(bookToUpdate);
         }
+
     }
 
 }

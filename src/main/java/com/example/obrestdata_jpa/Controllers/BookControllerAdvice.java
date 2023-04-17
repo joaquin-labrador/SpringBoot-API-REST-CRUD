e3e23c900a81e02fefed0986f0dec2a527f44fbc
joaquin-labrador <joaquinlabrador1010@gmail.com>
2023-04-17 17:36:48 -0300

    Add handle not book create exception

--- src/main/java/com/example/obrestdata_jpa/Controllers/BookControllerAdvice.java
+++ src/main/java/com/example/obrestdata_jpa/Controllers/BookControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.example.obrestdata_jpa.Controllers;
 
+import com.example.obrestdata_jpa.Error.BookNotCreateException;
 import com.example.obrestdata_jpa.Error.BookNotFoundException;
 import com.example.obrestdata_jpa.Error.ErrorResponse;
 import org.springframework.http.HttpStatus;
@@ -9,9 +10,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class BookControllerAdvice {
+
+
+    @ExceptionHandler(BookNotCreateException.class)
+    public ResponseEntity<ErrorResponse> handleBookNotCreateException(BookNotCreateException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+
     @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleBookNotFoundException(BookNotFoundException ex) {
-        ErrorResponse errorResponse = new ErrorResponse("Libro no encontrado", HttpStatus.NOT_FOUND.value());
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 

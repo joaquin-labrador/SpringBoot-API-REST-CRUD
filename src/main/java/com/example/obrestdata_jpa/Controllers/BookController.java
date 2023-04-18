e1bfc27309b012042d2ead97f59330e6f55295ea
joaquin-labrador <joaquinlabrador1010@gmail.com>
2023-04-17 22:07:52 -0300

    Add Delete Book by Id

--- src/main/java/com/example/obrestdata_jpa/Controllers/BookController.java
+++ src/main/java/com/example/obrestdata_jpa/Controllers/BookController.java
@@ -2,8 +2,8 @@ package com.example.obrestdata_jpa.Controllers;
 
 import ch.qos.logback.classic.Logger;
 import com.example.obrestdata_jpa.Entities.Book;
-import com.example.obrestdata_jpa.Error.BookNotCreateException;
 import com.example.obrestdata_jpa.Error.BookNotFoundException;
+import com.example.obrestdata_jpa.Error.InternalServerError;
 import com.example.obrestdata_jpa.Services.BookService;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +14,7 @@ import java.util.UUID;
 
 @RestController // This means that this class is a Controller
 public class BookController {
-    private final Logger logger = (Logger) LoggerFactory.getLogger(BookController.class);
+    private final Logger log = (Logger) LoggerFactory.getLogger(BookController.class);
     private final BookService bookService;
 
     //Inject dependencies:
@@ -39,6 +39,7 @@ public class BookController {
     public ResponseEntity<List<Book>> findAll() {
         List<Book> books = this.bookService.findAll();
         if (books.isEmpty()) {
+            log.warn("Books not found");
             throw new BookNotFoundException("Books not found");
         } else {
             return ResponseEntity.ok(books);
@@ -57,6 +58,7 @@ public class BookController {
 
         Book book = this.bookService.findById(id);
         if (book == null) {
+            log.warn("Book not found with id: " + id);
             throw new BookNotFoundException("Book not found with id: " + id);
         } else {
             return ResponseEntity.ok(book);
@@ -71,10 +73,11 @@ public class BookController {
      */
     @PostMapping("/api/books")
     @ResponseBody
-    public ResponseEntity<Book> create(@RequestBody Book book) {
+    public ResponseEntity<Book> create(@RequestBody Book book) throws Exception {
         Book newBook = ResponseEntity.ok(this.bookService.save(book)).getBody();
         if (newBook == null) {
-            throw new BookNotCreateException("Book not created, internal server error");
+            log.error("Internal Server Error");
+            throw new InternalServerError("Book not created, please try again");
         } else {
             return ResponseEntity.ok(book);
         }
@@ -92,11 +95,25 @@ public class BookController {
 
         Book bookToUpdate = this.bookService.update(id, book);
         if (bookToUpdate == null) {
+            log.warn("Book not found with id: " + id);
             throw new BookNotFoundException("Book not found with id: " + id);
         } else {
             return ResponseEntity.ok(bookToUpdate);
         }
 
+    }
+
+    /*
+     * HTTP DELETE: /api/books/{id}
+     * @param id
+     * @return Book
+     */
+    @DeleteMapping("/api/books/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        this.bookService.delete(id);
+        log.info("Book deleted with id: " + id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/example/obrestdata_jpa/Services/BookService.java
+++ src/main/java/com/example/obrestdata_jpa/Services/BookService.java
@@ -1,7 +1,11 @@
 package com.example.obrestdata_jpa.Services;
 
+import ch.qos.logback.classic.Logger;
 import com.example.obrestdata_jpa.Entities.Book;
+import com.example.obrestdata_jpa.Error.BookBadRequestException;
+import com.example.obrestdata_jpa.Error.BookNotFoundException;
 import com.example.obrestdata_jpa.Repositories.BookRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,7 +14,8 @@ import java.util.UUID;
 @Service
 public class BookService {
 
-    private BookRepository bookRepository;
+    private final Logger log = (Logger) LoggerFactory.getLogger(BookService.class);
+    private final BookRepository bookRepository;
 
     //Inject dependencies:
     public BookService(BookRepository bookRepository) {
@@ -40,24 +45,56 @@ public class BookService {
      * @return Book
      */
     public Book save(Book book) {
+        if (book.getId() != null) {
+            log.warn("Trying to create a book with an id");
+            throw new BookBadRequestException("The id created automatically, you can't set it");
+        }
+
         return this.bookRepository.save(book);
     }
 
     /*
-     * @param id , book
-     * @return Book or null
+     * @params id , book
+     * @return Book
      */
     public Book update(UUID id, Book book) {
-        Book bookToUpdate = this.bookRepository.findById(id).orElse(null);
-        if (bookToUpdate != null) {
-            bookToUpdate.setTitle(book.getTitle());
-            bookToUpdate.setPages(book.getPages());
-            bookToUpdate.setPrice(book.getPrice());
-            bookToUpdate.setReleaseDate(book.getReleaseDate());
-            bookToUpdate.setEbook(book.getEbook());
-            return this.bookRepository.save(bookToUpdate);
+
+        if (id == null) {
+            log.warn("Trying to update a book with a null id");
+            throw new BookBadRequestException("The id can't be null");
         }
-        return null;
+
+        Book bookToUpdate = this.bookRepository.findById(id).orElse(null);
+        if (bookToUpdate == null) {
+            log.warn("The book with id: " + id + " not found");
+            throw new BookNotFoundException("Book not found");
+        }
+        bookToUpdate.setTitle(book.getTitle());
+        bookToUpdate.setPages(book.getPages());
+        bookToUpdate.setPrice(book.getPrice());
+        bookToUpdate.setReleaseDate(book.getReleaseDate());
+        bookToUpdate.setEbook(book.getEbook());
+        return this.bookRepository.save(bookToUpdate);
+
+
     }
 
+
+    /*
+     * @param id
+     * @return book
+     * */
+    public void delete(UUID id) {
+        if (id == null) {
+            log.warn("Not have id for found de book to delete");
+            throw new BookBadRequestException("The id can't be null");
+        }
+
+        if (!this.bookRepository.existsById(id)) {
+            log.warn("The book with id: " + id + " not found");
+            throw new BookNotFoundException("Book not found");
+        }
+
+        this.bookRepository.deleteById(id);
+    }
 }

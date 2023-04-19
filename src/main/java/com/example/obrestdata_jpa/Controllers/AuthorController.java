36473d65b60be5807f31a680ff165bf3378ac18d
joaquin-labrador <joaquinlabrador1010@gmail.com>
2023-04-19 18:59:12 -0300

    Add Controller and Service for Author REST

---
+++ src/main/java/com/example/obrestdata_jpa/Controllers/AuthorController.java
@@ -0,0 +1,92 @@
+package com.example.obrestdata_jpa.Controllers;
+
+import ch.qos.logback.classic.Logger;
+import com.example.obrestdata_jpa.Entities.Author;
+import com.example.obrestdata_jpa.Error.InternalServerError;
+import com.example.obrestdata_jpa.Error.NotFoundException;
+import com.example.obrestdata_jpa.Services.AuthorService;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class AuthorController {
+
+    private final AuthorService authorService;
+    private final Logger log = (Logger) LoggerFactory.getLogger(BookController.class);
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    /*
+     * HTTP GET: /api/authors
+     * @return ResponseEntity<List<Author>>
+     */
+    @GetMapping("/api/authors")
+    @ResponseBody
+    public ResponseEntity<List<Author>> findAll() {
+
+        List<Author> authors = this.authorService.findAll();
+        if (authors.isEmpty()) {
+            log.warn("Authors not found");
+            throw new NotFoundException("Authors not found");
+        } else {
+            return ResponseEntity.ok(authors);
+        }
+
+    }
+
+    /*
+     * HTTP GET: /api/authors/{id}
+     * @params id
+     * @return ResponseEntity<Author>
+     */
+    @GetMapping("/api/authors/{id}")
+    @ResponseBody
+    public ResponseEntity<Author> findById(@PathVariable Long id) {
+
+        Author author = this.authorService.findById(id);
+        if (author == null) {
+            log.warn("Author not found");
+            throw new NotFoundException("Author not found");
+        }
+        return ResponseEntity.ok(author);
+
+
+    }
+
+    /*
+     * HTTP POST: /api/authors
+     * @params author
+     * @return ResponseEntity<Author>
+     */
+    @PostMapping("/api/authors")
+    @ResponseBody
+    public ResponseEntity<Author> create(@RequestBody Author author) {
+        Author newAuthor = this.authorService.save(author);
+        if (newAuthor == null) {
+            log.warn("Internal Server Error");
+            throw new InternalServerError("Author not created");
+        }
+        return ResponseEntity.ok(author);
+
+
+    }
+
+    /*
+     * HTTP PUT: /api/authors/{id}
+     * @params author, id
+     * @return ResponseEntity<Author>
+     */
+    @PutMapping("/api/authors/{id}")
+    @ResponseBody
+    public ResponseEntity<Author> update(@RequestBody Author author, @PathVariable Long id) {
+        Author updateAuthor = this.authorService.update(author, id);
+        return ResponseEntity.ok(updateAuthor);
+    }
+
+
+}
---
+++ src/main/java/com/example/obrestdata_jpa/Services/AuthorService.java
@@ -0,0 +1,81 @@
+package com.example.obrestdata_jpa.Services;
+
+import ch.qos.logback.classic.Logger;
+import com.example.obrestdata_jpa.Entities.Author;
+import com.example.obrestdata_jpa.Error.BadRequestException;
+import com.example.obrestdata_jpa.Error.NotFoundException;
+import com.example.obrestdata_jpa.Repositories.AuthorRepository;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthorService {
+    private final AuthorRepository authorRepository;
+    private final Logger log = (Logger) LoggerFactory.getLogger(BookService.class);
+
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    /*
+     * @return List<Author>
+     */
+    public List<Author> findAll() {
+        return this.authorRepository.findAll();
+    }
+
+    /*
+     * @params id
+     * @return Author or Null
+     */
+    public Author findById(Long id) {
+        return this.authorRepository.findById(id).orElse(null);
+    }
+
+    /*
+     * @params author
+     * @return Author or Null
+     */
+    public Author save(Author author) {
+        try {
+            if (author.getId() != null) {
+                log.warn("Not create a author with an id");
+                throw new BadRequestException("The id created automatically, you can't set it");
+            }
+            return this.authorRepository.save(author);
+        } catch (DataIntegrityViolationException exception) {
+            log.warn("The name already exist");
+            throw new BadRequestException("The author already exist in the data base");
+        }
+    }
+
+    /*
+     * @params author , id
+     * @return Author
+     */
+    public Author update(Author author, Long id) {
+        try {
+
+            Author authorToUpdate = this.authorRepository.findById(id).orElse(null);
+
+            if (authorToUpdate == null) {
+                log.error("Author not found");
+                throw new NotFoundException("Author not found");
+            }
+
+            authorToUpdate.setCompleteName(author.getCompleteName());
+            authorToUpdate.setBirthDate(author.getBirthDate());
+            authorToUpdate.setCountry(author.getCountry());
+            authorToUpdate.setDeathDate(author.getDeathDate());
+
+            return this.authorRepository.save(authorToUpdate);
+        } catch (DataIntegrityViolationException exception) {
+            log.warn("The name already exist");
+            throw new BadRequestException("The author already exist in the data base");
+        }
+
+    }
+}

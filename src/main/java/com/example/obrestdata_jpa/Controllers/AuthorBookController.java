384337c59b089c6f1cb7f28c21c5ffe5742ac230
joaquin-labrador <joaquinlabrador1010@gmail.com>
2023-05-01 23:30:08 -0300

    Add HTTP petition return all books and it's books

--- src/main/java/com/example/obrestdata_jpa/Controllers/AuthorBookController.java
+++ src/main/java/com/example/obrestdata_jpa/Controllers/AuthorBookController.java
@@ -1,2 +1,35 @@
-package com.example.obrestdata_jpa.Controllers;public class AuthorBookController {
+package com.example.obrestdata_jpa.Controllers;
+
+import com.example.obrestdata_jpa.DTO.AuthorBookDTO;
+import com.example.obrestdata_jpa.Error.NotFoundException;
+import com.example.obrestdata_jpa.Services.AuthorBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AuthorBookController {
+
+    private final AuthorBookService authorBookService;
+
+
+    public AuthorBookController(AuthorBookService authorBookService) {
+        this.authorBookService = authorBookService;
+    }
+
+    @GetMapping("/api/authors-books")
+    @ResponseBody
+    public ResponseEntity<List<AuthorBookDTO>> getAllAuthorsAndBooks() {
+        List<AuthorBookDTO> authorBookDTOList = this.authorBookService.getAllAuthorsWithBooks();
+        if (authorBookDTOList.isEmpty()) {
+            throw new NotFoundException("Authors and books not found");
+        } else {
+            return ResponseEntity.ok(authorBookDTOList);
+        }
+
+    }
 }
--- src/main/java/com/example/obrestdata_jpa/Services/AuthorBookService.java
+++ src/main/java/com/example/obrestdata_jpa/Services/AuthorBookService.java
@@ -1,2 +1,40 @@
-package com.example.obrestdata_jpa.Services;public class AuthorBookService {
+package com.example.obrestdata_jpa.Services;
+
+import com.example.obrestdata_jpa.DTO.AuthorBookDTO;
+import com.example.obrestdata_jpa.DTO.AuthorDTO;
+import com.example.obrestdata_jpa.DTO.BookDTO;
+import com.example.obrestdata_jpa.Entities.Author;
+import com.example.obrestdata_jpa.Repositories.AuthorRepository;
+import com.example.obrestdata_jpa.Repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthorBookService {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public AuthorBookService(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    public List<AuthorBookDTO> getAllAuthorsWithBooks() {
+        List<Author> authors = this.authorRepository.findAll();
+        List<AuthorBookDTO> authorBookDTOList = authors.stream().map(author -> {
+                    AuthorDTO authorDTO = new AuthorDTO(author);
+                    List<BookDTO> bookDTOList = bookRepository.findBookByAuthorId(author.getId())
+                            .stream()
+                            .map(BookDTO::new)
+                            .toList();
+                    return new AuthorBookDTO(authorDTO, bookDTOList);
+                })
+                .toList();
+        System.out.println(authorBookDTOList);
+        return authorBookDTOList;
+
+    }
 }

b1b8e36f0b1499a998b75a90876bf51167c62173
joaquin-labrador <joaquinlabrador1010@gmail.com>
2023-05-01 23:41:50 -0300

    Add a jpa transaction query syntax method

--- src/main/java/com/example/obrestdata_jpa/Repositories/BookRepository.java
+++ src/main/java/com/example/obrestdata_jpa/Repositories/BookRepository.java
@@ -3,8 +3,11 @@ package com.example.obrestdata_jpa.Repositories;
 import com.example.obrestdata_jpa.Entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    List<Book> findBookByAuthorId(Long id);
 
 }

18ba5f88dca4366ee0596fe7284bdc4dff377a98
joaquin-labrador <joaquinlabrador1010@gmail.com>
2023-05-01 23:43:14 -0300

    code format

--- src/main/java/com/example/obrestdata_jpa/Services/AuthorBookService.java
+++ src/main/java/com/example/obrestdata_jpa/Services/AuthorBookService.java
@@ -6,7 +6,6 @@ import com.example.obrestdata_jpa.DTO.BookDTO;
 import com.example.obrestdata_jpa.Entities.Author;
 import com.example.obrestdata_jpa.Repositories.AuthorRepository;
 import com.example.obrestdata_jpa.Repositories.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;

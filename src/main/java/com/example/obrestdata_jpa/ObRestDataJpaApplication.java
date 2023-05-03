650a1600d54a448e11feb40a34c239482c10bc88
joaquin-labrador <joaquinlabrador1010@gmail.com>
2023-05-02 21:16:40 -0300

    Fix modularize seed function

--- src/main/java/com/example/obrestdata_jpa/ObRestDataJpaApplication.java
+++ src/main/java/com/example/obrestdata_jpa/ObRestDataJpaApplication.java
@@ -1,8 +1,8 @@
 package com.example.obrestdata_jpa;
 
-import com.example.obrestdata_jpa.Enums.Country;
 import com.example.obrestdata_jpa.Entities.Author;
 import com.example.obrestdata_jpa.Entities.Book;
+import com.example.obrestdata_jpa.Enums.Country;
 import com.example.obrestdata_jpa.Repositories.AuthorRepository;
 import com.example.obrestdata_jpa.Repositories.BookRepository;
 import org.springframework.boot.SpringApplication;
@@ -14,38 +14,52 @@ import java.util.ArrayList;
 
 @SpringBootApplication
 public class ObRestDataJpaApplication {
-
-    public static void main(java.lang.String[] args) {
+    public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(ObRestDataJpaApplication.class, args);
-        BookRepository bookRepository = context.getBean(BookRepository.class);
-        AuthorRepository authorRepository = context.getBean(AuthorRepository.class);
-
-        //CRUD
-        //Create:
-        Book bookOne = new Book(null, "Java 8", 1000, 100.0, LocalDate.now(), true);
-        Book bookTwo = new Book(null, "Clean Code", 464, 40.00, LocalDate.of(2008, 8, 1), true);
-
-        Author authorOne = new Author(null, "Robert C. Martin",  LocalDate.of(1952, 12, 5), LocalDate.of(1999, 5, 6), Country.USA.getUrl());
-        Author authorTwo = new Author(null, "Joshua Bloch",  LocalDate.of(1961, 8, 28), null, Country.USA.getUrl());
-
-
-        ArrayList<Book> booksList = new ArrayList<>();
-        booksList.add(bookOne);
-        booksList.add(bookTwo);
-
-        ArrayList<Author> authorsList = new ArrayList<>();
-        authorsList.add(authorOne);
-        authorsList.add(authorTwo);
-        //Save in DB:
-        bookRepository.saveAll(booksList);
-        authorRepository.saveAll(authorsList);
-
-
-        //Read:
-        System.out.println("Authors: ");
-        authorRepository.findAll().forEach(System.out::println);
+        seedDataBase(context);
 
     }
 
 
+    public static void seedDataBase(ApplicationContext context) {
+        BookRepository bookRepository = context.getBean(BookRepository.class);
+        AuthorRepository authorRepository = context.getBean(AuthorRepository.class);
+
+        //CRUD
+        // Create books
+        Book bookOne = new Book(null, "Java 8", 1000, 100.0, LocalDate.now(), true);
+        Book bookTwo = new Book(null, "Clean Code", 464, 40.00, LocalDate.of(2008, 8, 1), true);
+        Book bookThree = new Book(null, "The Pragmatic Programmer", 352, 50.0, LocalDate.of(1999, 10, 20), true);
+
+        // Create authors
+        Author authorOne = new Author(null, "Robert C. Martin", LocalDate.of(1952, 12, 5), LocalDate.of(1999, 5, 6), Country.USA.getUrl());
+        Author authorTwo = new Author(null, "Joshua Bloch", LocalDate.of(1961, 8, 28), null, Country.USA.getUrl());
+        Author authorThree = new Author(null, "Andrew Hunt", LocalDate.of(1964, 3, 4), null, Country.USA.getUrl());
+
+        ArrayList<Author> authorsList = new ArrayList<>();
+        authorsList.add(authorOne);
+        authorsList.add(authorTwo);
+        authorsList.add(authorThree);
+
+        // Save in DB
+        authorRepository.saveAll(authorsList);
+
+        ArrayList<Book> booksList = new ArrayList<>();
+        booksList.add(bookOne);
+        booksList.add(bookTwo);
+        booksList.add(bookThree);
+
+        // Save books
+        bookRepository.saveAll(booksList);
+
+        // Associate authors with books
+        bookOne.setAuthor(authorOne);
+        bookTwo.setAuthor(authorOne);
+        bookThree.setAuthor(authorThree);
+
+        //Update books
+        bookRepository.save(bookOne);
+        bookRepository.save(bookTwo);
+        bookRepository.save(bookThree);
+    }
 }

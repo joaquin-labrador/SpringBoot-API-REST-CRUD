4af1f828b6329feaa52cce40b4c3873e015fbcdf
joaquin-labrador <joaquinlabrador1010@gmail.com>
2023-05-05 18:34:40 -0300

    Refactor apply ApiErrorResponse

--- src/main/java/com/example/obrestdata_jpa/Controllers/ControllerAdvice.java
+++ src/main/java/com/example/obrestdata_jpa/Controllers/ControllerAdvice.java
@@ -1,7 +1,7 @@
 package com.example.obrestdata_jpa.Controllers;
 
+import com.example.obrestdata_jpa.Exceptions.ApiErrorResponse;
 import com.example.obrestdata_jpa.Exceptions.BadRequestException;
-import com.example.obrestdata_jpa.Exceptions.ErrorResponse;
 import com.example.obrestdata_jpa.Exceptions.InternalServerError;
 import com.example.obrestdata_jpa.Exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
@@ -9,26 +9,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public final class ControllerAdvice {
 
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex) {
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+    public ResponseEntity<ApiErrorResponse> handleBadRequestException(BadRequestException ex) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value(), LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex) {
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+    public ResponseEntity<ApiErrorResponse> handleNotFoundException(NotFoundException ex) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value(), LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
     @ExceptionHandler(InternalServerError.class)
-    public ResponseEntity<ErrorResponse> handleInternalServerError(InternalServerError ex) {
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+    public ResponseEntity<ApiErrorResponse> handleInternalServerError(InternalServerError ex) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value(), LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 
